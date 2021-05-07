ad781b7e1eb15f91c0770c56b051062d1d3e99c5
haasman <josh@haasresearch.com>
2021-05-07 11:09:44 -0400

    fix cont imports

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = 3000;
-const { execFile } = require("child_process");
-const { execFileSync } = require("child_process").execFileSync;
+
+const { 
+  spawnSync, 
+  execSync, 
+  execFileSync,
+} = require('child_process');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
 
-  // var testy = execFile("python3", ["slow.py"]);
   var testy = execFileSync("python3", ["slow.py"]);
+  // var testy = execSync("python3", ["slow.py"]);
 
-  res.end("done");
+  // const stdout = execSync("python3 slow.py");
+
+  res.end(testy);
 });
 
 server.listen(port, hostname, () => {
